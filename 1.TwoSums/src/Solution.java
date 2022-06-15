4fe9cc17b65cd0d094a4db92ec001331efd7abbf
Abler31 <58187511+Abler31@users.noreply.github.com>
2022-06-15 12:25:20 +0300

    1.Two sums

---
+++ 1.TwoSums/src/Solution.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            int requiredNumber = target - nums[i];
+
+            if (map.containsKey(requiredNumber) && map.get(requiredNumber) != i) {
+                return new int[]{i, map.get(requiredNumber)};
+            }
+        }
+        throw new IllegalArgumentException("Not found");
+    }
+}
