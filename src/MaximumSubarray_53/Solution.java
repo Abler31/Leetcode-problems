70bf188ebd1c132caa79b316cd529797b697cdfc
Abler31 <58187511+Abler31@users.noreply.github.com>
2022-06-20 11:41:02 +0300

    53

---
+++ src/MaximumSubarray_53/Solution.java
@@ -0,0 +1,13 @@
+package MaximumSubarray_53;
+
+public class Solution {
+    public int maxSubArray(int[] nums) {
+        int currentSum = nums[0];
+        int maxSum = currentSum;
+        for(int i = 1; i<nums.length; i++){
+            currentSum = Math.max(nums[i] + currentSum, nums[i]);
+            maxSum = Math.max(currentSum, maxSum);
+        }
+        return maxSum;
+    }
+}
